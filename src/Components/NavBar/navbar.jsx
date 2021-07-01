14035fa6aca3d9b9e5a9365af460c4ff2cdcbd0f
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-07-01 16:22:43 -0500

    added in navbar conditional rendering for user logged in or not

--- src/Components/NavBar/navbar.jsx
+++ src/Components/NavBar/navbar.jsx
@@ -12,8 +12,10 @@ class NavBar extends Component{
     }
 
     render(){
-        return(
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        if (this.props.user.length != 0){
+            console.log(this.props.user.items)
+            return(
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
@@ -25,16 +27,6 @@ class NavBar extends Component{
                                 GameOn
                             </li>
                         </Link>
-                        <Link to="/login" class="nav-link">
-                            <li class="navbar-brand nav-item">
-                                Login
-                            </li>
-                        </Link>
-                        <Link to="/registration" class="nav-link">
-                            <li class="navbar-brand nav-item">
-                                SignUp
-                            </li>
-                        </Link>
                         <Link to="/profile" class="nav-link">
                             <li class="navbar-brand nav-item">
                                 Profile
@@ -44,7 +36,39 @@ class NavBar extends Component{
                     </ul>
                 </div>
             </nav>
-        );
+            )
+        }
+        else {
+            console.log(this.props.user.items)
+            return(
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <Link to="/" class="nav-link">
+                                <li class="navbar-brand nav-item">
+                                    <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt=""/>
+                                    GameOn
+                                </li>
+                            </Link>
+                            <Link to="/login" class="nav-link">
+                                <li class="navbar-brand nav-item">
+                                    Login
+                                </li>
+                            </Link>
+                            <Link to="/registration" class="nav-link">
+                                <li class="navbar-brand nav-item">
+                                    SignUp
+                                </li>
+                            </Link>
+                        </ul>
+                    </div>
+                </nav>
+            );
+        }
+        
     }
 }
 
@@ -52,4 +76,8 @@ NavBar.propTypes = {
     logout: PropTypes.func.isRequired
 };
 
-export default connect(null, { logout })(NavBar);
+const mapStateToProps = state => ({
+    user: state.user.items,
+});
+
+export default connect(mapStateToProps, { logout })(NavBar);
