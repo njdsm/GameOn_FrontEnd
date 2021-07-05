5ccebce89c1443474e7cfb36d216222fbba8d2ce
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-07-05 15:00:04 -0500

    fixed a bug that was setting current game to players

--- src/actions/currentGameActions.js
+++ src/actions/currentGameActions.js
@@ -5,12 +5,11 @@ import { START_GAME, END_GAME, JOIN_GAME, GET_PLAYERS, ADD_PLAYER } from './type
 //thunk middleware allows us to call dispatch function directly so we can make asynchronous requests
 //dispatch is like resolving a promise; dispatch allows for sending of data
 export const startGame = (user, game) => dispatch => {
-    debugger
     axios.put('http://127.0.0.1:8000/games/' + game.id + "/", {"is_active": true}).then(
-            game => dispatch({
-                type: START_GAME,
-                payload: game,
-            })
+        game => dispatch({
+            type: START_GAME,
+            payload: game,
+        })
     );
     axios.get('http://127.0.0.1:8000/current_game/').then(
         players => dispatch({
